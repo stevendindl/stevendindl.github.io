f850019f5709e44fcbe5edb330ad0aaf81cc0d48
stevendindl <sdindl@email.sc.edu>
2025-05-17 23:11:25 -0400

    Fixed "base" in vite config

--- portfolio-2025/vite.config.js
+++ portfolio-2025/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/portfolio-2025/', 
+  base: 'https://stevendindl.github.io/portfolio-2025/', 
   plugins: [react()],
 })
